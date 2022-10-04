1fe207f5c415e899c961377a943e766bed53d253
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-10-04 14:49:54 +0200

    Create LinkTransformation.php

---
+++ Classes/Transformation/LinkTransformation.php
@@ -0,0 +1,50 @@
+<?php
+namespace Checkitsedo\TelegramRss\Transformation;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\SingletonInterface;
+
+/**
+ * Example transformation functions for the 'telegram_rss' extension
+ *
+ * @author Francois Suter (Idéative) <typo3@ideative.ch>
+ * @package TYPO3
+ * @subpackage tx_telegramrss
+ */
+class LinkTransformation implements SingletonInterface
+{
+
+    /**
+     * Parses a rich-text content to make links absolute.
+     *
+     * Somehow the Core's link parser chokes on relative links, which should not happen.
+     * This is a workaround rather than digging in the Core.
+     *
+     * @param array $record The full record that is being transformed
+     * @param mixed $index The index of the field to transform
+     * @param array $params Additional parameters from the TCA
+     * @return string The text with modified links
+     */
+    public function absolutizeUrls(array $record, $index, array $params): string
+    {
+        $host = $params['host'];
+        $text = $record[$index] ?? '';
+        return str_replace(
+                'href="/',
+                'href="' . $host . '/',
+                $text
+        );
+    }
+}
