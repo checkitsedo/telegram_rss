01e04a387fce6f231388b6bd2fc7d47bed436d8d
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-10-04 14:53:13 +0200

    Create tx_news_domain_model_link.php

---
+++ Configuration/TCA/Overrides/tx_news_domain_model_link.php
@@ -0,0 +1,62 @@
+<?php
+
+// Add the external general information
+$GLOBALS['TCA']['tx_news_domain_model_link']['external']['general'] = [
+    0 => [
+        'connector' => 'feed',
+        'parameters' => [
+            'uri' => 'https://typo3.org/?type=100'
+        ],
+        'data' => 'xml',
+        'nodetype' => 'item',
+        'referenceUid' => 'uri',
+        'enforcePid' => true,
+        'priority' => 210,
+        'group' => 'telegram_rss',
+        'disabledOperations' => 'delete',
+        'description' => 'Import of typo3.org news related links'
+    ],
+];
+// Add the external information for each column
+$GLOBALS['TCA']['tx_news_domain_model_link']['columns']['title']['external'] = [
+    0 => [
+        'field' => 'title'
+    ]
+];
+$GLOBALS['TCA']['tx_news_domain_model_link']['columns']['uri']['external'] = [
+    0 => [
+        'field' => 'link',
+        'transformations' => [
+            10 => [
+                'trim' => true
+            ]
+        ]
+    ]
+];
+$GLOBALS['TCA']['tx_news_domain_model_link']['columns']['parent'] = [
+    'config' => [
+        'type' => 'passthrough',
+    ],
+    'external' => [
+        0 => [
+            'field' => 'link',
+            'transformations' => [
+                10 => [
+                    'trim' => true
+                ],
+                20 => [
+                    'mapping' => [
+                        'table' => 'tx_news_domain_model_news',
+                        'referenceField' => 'tx_telegramrss_externalid',
+                        'default' => 0
+                    ]
+                ],
+                30 => [
+                    'isEmpty' => [
+                        'invalidate' => true
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
